5abd4d671b023a880dceb31b9bf591784574741a
RisingNova <narongkorn2540@gmail.com>
2021-03-24 14:50:36 +0700

    model done

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const { sequelize } = require("./models");
+const customerRoute = require("./Routes/customerRouter");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/customers", customerRoute);
 app.use((req, res, next) => {
   res.status(404).json({ message: "Page not Found bitch!" });
 });
@@ -9,5 +13,6 @@ app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).json({ message: err.message });
 });
+// sequelize.sync({ force: true });
 const port = 8000;
-app.listen(port, () => console.log(`server is running in ${port}`));
+app.listen(port, () => console.log(`server is running on ${port}`));
